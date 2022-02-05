06247a7eb4098f92471c72650c57e1128476eded
Michelle Huang <michelleahuang@berkeley.edu>
2022-02-05 13:59:23 -0800

    Add routes

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require("express");
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
 
 
 const app = express();
@@ -11,5 +16,11 @@ mongoose
     .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello Beautiful World!"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
