bb218e1fdd8e1804ac988c1ffb5dd33c41bfaabc
Michelle Huang <michelleahuang@berkeley.edu>
2022-02-05 15:53:06 -0800

    Working on user auth

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+
 
 
 const users = require("./routes/api/users");
@@ -15,7 +17,10 @@ mongoose
     .then(() => console.log("Connected to MongoDB successfully"))
     .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello Beautiful World!"));
+// app.get("/", (req, res) => res.send("Hello Beautiful World!"));
+
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
